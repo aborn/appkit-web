fb0babf9f2a4148610c5316efb06c96f9c0f54dc
蒋国宝 <loveaborn@foxmail.com>
2015-09-29 00:11:39 +0800

    install

--- install.sh
+++ install.sh
@@ -13,7 +13,8 @@ rm -rf ~/software/apache-tomcat-7.0.61/webapps/appkit-web
 cp -r target/appkit-web ~/software/apache-tomcat-7.0.61/webapps/
 
 # stop tomcat and start it
-./catalina.sh stop
-./catalina.sh start
+# ~/software/apache-tomcat-7.0.61/bin/catalina.sh stop
+kill -9 `ps -ef |grep tomcat|grep -v grep|awk '{print $2}'`
+~/software/apache-tomcat-7.0.61/bin/catalina.sh start
 
 
